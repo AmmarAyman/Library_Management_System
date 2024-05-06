5d69214848206f084d02668e764abaddf876184f
omarabohammer <136636395+omarabohammer@users.noreply.github.com>
2024-05-06 18:28:40 -0200

    Update StudentRecordManager.h
    student record

--- library/StudentRecordManager.h
+++ library/StudentRecordManager.h
@@ -5,10 +5,16 @@
 using namespace std;
 
 class StudentRecordManager { // Not Complete
+private:
 	string studentName;
 	string department;
 	int studentID;
 	int bookID;
+        
+public:
+        void issueRecord(){
+		
+
 
 };
 
